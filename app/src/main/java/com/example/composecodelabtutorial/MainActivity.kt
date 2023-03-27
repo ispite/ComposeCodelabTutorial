fc1f6beb04a4c41d48b59d47ca31dd38d5a66023
Denis Solovyov <infinityx93@gmail.com>
2023-03-27 11:27:15 +0300

    finished the 12th point

--- app/src/main/java/com/example/composecodelabtutorial/MainActivity.kt
+++ app/src/main/java/com/example/composecodelabtutorial/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composecodelabtutorial
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
@@ -93,7 +95,11 @@ private fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium
+                        .copy(fontWeight = FontWeight.ExtraBold)
+                )
             }
             ElevatedButton(onClick = { expanded.value = !expanded.value }) {
                 Text(if (expanded.value) "Show less" else "Show more")
@@ -102,11 +108,17 @@ private fun Greeting(name: String) {
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "dark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 private fun DefaultPreview() {
     ComposeCodelabTutorialTheme {
-        MyApp()
+        Greetings()
     }
 }
 
